e7564ee28b0b84f42ec69bdcf591181fdaa1aa9e
Artur Yanchenko <kayman3108@gmail.com>
2026-03-16 22:21:21 +0300

    Refactor: apply instructor's suggestions HW7

--- LevelUp.py
+++ LevelUp.py
@@ -1,9 +1,8 @@
 
 experience = 10
 threshold = 15
-reward = 4
+reward = 6
 
 new_lvl = experience + reward
 is_promoted = new_lvl >= threshold
-result = is_promoted
-print("Reached a new level?", result)
+print("Reached a new level?", is_promoted)
