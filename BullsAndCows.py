71eb1496400cd3c601019fedd9263ba66b7c91ce
Artur Yanchenko <kayman3108@gmail.com>
2026-03-21 21:03:48 +0300

    “Fixes for homework8

--- BullsAndCows.py
+++ BullsAndCows.py
@@ -1,36 +1,47 @@
 import random
+import string
 
 
 def gen_number():
-    numbers = "0123456789"
-    rand_number = random.sample(numbers, 4)
-    secret_num = "".join(rand_number)
-    return secret_num
+    numbers = string.digits
+    return "".join(random.sample(numbers, 4))
 
 
 def user_guess():
-    guess = input("Enter 4 numbers:")
-    return guess
+    while True:
+        guess = input("Enter 4 digits: ")
+
+        if not guess.isdigit():
+            continue
+        if len(guess) != 4:
+            continue
+        if len(set(guess)) != 4:
+            continue
+
+        return guess
 
 
 def check_num(secret_num, guess):
     bulls_count = 0
     cows_count = 0
-    for i in range(4):
-        if guess[i] == secret_num[i]:
+
+    for i, digit in enumerate(secret_num):
+        if guess[i] == digit:
             bulls_count += 1
         elif guess[i] in secret_num:
             cows_count += 1
+
     return bulls_count, cows_count
 
 
 secret = gen_number()
 
-
 while True:
     attempt = user_guess()
     bulls, cows = check_num(secret, attempt)
-    print(f"Bulls: {bulls},Cows:{cows}")
+
+    print(f"Bulls: {bulls}, Cows: {cows}")
+
     if bulls == 4:
         print("Win")
         break
