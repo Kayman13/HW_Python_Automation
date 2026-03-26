42b2977cbb5a262da2f24affa49bba7a2ea93126
Artur Yanchenko <kayman3108@gmail.com>
2026-03-26 22:25:31 +0300

    HW10 ready

---
+++ candles.py
@@ -0,0 +1,19 @@
+
+def solution(candle_number: int, make_new: int) -> int:
+    total = candle_number
+    leftovers = candle_number
+
+    while leftovers >= make_new:
+        new_candles = leftovers // make_new
+        total += new_candles
+        leftovers = leftovers % make_new + new_candles
+
+    return total
+
+
+# tests
+assert solution(5, 2) == 9
+assert solution(1, 2) == 1
+assert solution(15, 4) == 19
+assert solution(13, 5) == 16
+assert solution(2, 3) == 2
---
+++ hashtag.py
@@ -0,0 +1,19 @@
+
+def solution(text: str) -> str:
+    stack = []
+
+    for char in text:
+        if char == "#":
+            if stack:
+                stack.pop()
+        else:
+            stack.append(char)
+
+    return "".join(stack)
+
+
+assert solution("a#bc#d") == "bd"
+assert solution("abc#d##c") == "ac"
+assert solution("abc##d######") == ""
+assert solution("#######") == ""
+assert solution("") == ""
