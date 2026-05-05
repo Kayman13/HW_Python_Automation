39124a90b3a16ebdfcfcc05e8901acbd57c70481
Artur Yanchenko <kayman3108@gmail.com>
2026-05-05 22:31:11 +0300

    fix task

--- task2.py
+++ task2.py
@@ -1,12 +1,17 @@
 
-def square_number():
-    num = int(input("Enter a number: "))
-    print("Square:", num ** 2)
+def square_number(num):
+    return num ** 2
 
 
-def check_even_odd():
-    num = int(input("Enter a number: "))
-    if num % 2 == 0:
-        print("Even")
-    else:
-        print("Odd")
+def is_even(num):
+    return num % 2 == 0
+
+
+num1 = int(input("Enter a number: "))
+
+print("Square:", square_number(num1))
+
+if is_even(num1):
+    print("Even")
+else:
+    print("Odd")
--- task5.py
+++ task5.py
@@ -1,9 +1,9 @@
 
-def is_palindrome():
-    num = input("Enter a number: ")
+def is_palindrome(num: str) -> bool:
     if num.startswith('-'):
         return False
     return num == num[::-1]
 
 
-print("Result:", is_palindrome())
+num1 = input("Enter a number: ")
+print("Result:", is_palindrome(num1))
